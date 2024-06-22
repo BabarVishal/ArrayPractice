0415e7087dc2e35bc0a961d64a5905da8b6fd09c
vishal <vishalbabar7351@gmail.com>
2024-06-22 20:04:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const myArray = ["Vishal", "Dinesh", "Ganesh"];
+console.log(myArray);
+
+const myNewArray = [
+    "Vishal",
+    "Dinesh",
+    "Ganesh"
+]
+   
+const myNumber = [1, 2, 3, 4, 5];
+let length = myNumber.length;
+console.log(length);
+
+ myNumber.toString();
+
+ myNumber.at(2) //Get a third element in array
+
+ myNumber.join("*"); // join the array like this 1*2*3*4*5
+
+ myNumber.pop(); // remove the last element form array 
+
+ myNumber.push("Vishal"); // add new element to the array in last
