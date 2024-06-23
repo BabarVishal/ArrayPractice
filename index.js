1a1b4cb330aa9ec901270d364bd4aeede94e5bcc
vishal babar <123831872+BabarVishal@users.noreply.github.com>
2024-06-23 19:46:25 +0530

    Update index.js
    added some extra method in array

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ console.log(length);
 
  myNumber.join("*"); // join the array like this 1*2*3*4*5
 
- myNumber.pop(); // remove the last element form array 
+myNumber.pop(); // remove the last element form array 
 
  myNumber.push("Vishal"); // add new element to the array in last
+
+ myNumber.shift("Vishal") // remove the fisth element in array
+
+ myNumber.unshift("Vishal")// add the element in fisth in array
+
+ const fisthNumber = [1,2,3,4,5];
+ const secondeNumber = [6,7,8,9,10];
+  let allNumber = fisthNumber.concat(secondeNumber);  //add both the array in one
+  console.log(allNumber);
